74e2f234f5b6bcd62e0ae541527ddaf74e69dfd9
unsivilaudio <unsivilaudio@gmail.com>
2022-04-27 11:24:17 -0600

    pre-06 update

--- 04-making_real_projects/simpleweb/index.js
+++ 04-making_real_projects/simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi there.');
+    res.send('Hello there frined.');
 });
 
 app.listen(8080, console.log.bind(null, 'Listening on port 8080'));
